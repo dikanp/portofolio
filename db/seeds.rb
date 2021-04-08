0f74dcd0f70b1ae75bc52f691ac5e130f7f66212
dikanp <andika.dikanp@gmail.com>
2021-04-08 19:56:44 +0700

    add and update

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |blog|
+    Blog.create!(
+        title: "My Blog #{blog}",
+        body: "#<ActiveRecord::Relation [#<Blog id: 1, title]>"
+    )
+end
+
+puts "10 blog posts created"
+
+5.times do |skill|
+    Skill.create!(
+        title: "Rails #{skill}",
+        percent_utilized: 15
+    )
+end
+
+puts "5 skills created"
+
+9.times do |portfolio_item|
+    Portfolio.create!(
+        title: "Portfolio title #{portfolio_item}",
+        subtitle: "My great service",
+        body: "oke",
+        main_image: "http://placehold.it/600x200",
+        thumb_image: "http://placehold.it/350x200"
+    )
+end
+
+puts "9 Portfolio items created"
