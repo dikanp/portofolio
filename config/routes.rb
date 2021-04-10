170998b0c29bfe0fe4626dcdaf31860665c0d104
dikanp <andika.dikanp@gmail.com>
2021-04-10 14:53:38 +0700

    validation and globbing missing

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,15 @@ Rails.application.routes.draw do
   get 'portfolios/:id', to: 'portfolios#show', as: 'portfolio_show'
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
-  resources :blogs
+
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
   
+  get '*missing', to: 'pages#missing'
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
